8d81f2772fdb4f648b66e307eea7d37e2e3ed90c
Rebekah <rbk1008@Gmail.com>
2021-12-31 18:54:34 -0700

    commit tree 2afa79224733d46c85b7728b8a1c08383126bc51

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
-class Person
-  # your code here
+class Person 
+
+  def initialize(attributes = nil)
+    if attributes
+      attributes.each do |key, value|
+        self.class.attr_accessor(key)
+        self.send("#{key}=", value)
+      end
+    end
+  end
 end
